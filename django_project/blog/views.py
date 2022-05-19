1406ae8e6bc6d11beaa55cfdf341b4f88fa3123f
Khair Ahammed <khairahmad6@gmail.com>
2022-05-20 02:13:13 +0600

    lecture 3,4

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+posts = [
+    {
+        'post_author': 'Khair',
+        'post_title': 'Is Machine Learning the future ?',
+        'post_time': '18/5/2022',
+        'content': 'sasgfhjkkkkkkkkkkklkafaf'
+    },
+    {
+        'post_author': 'Ahammed',
+        'post_title': 'Data Analyst vs Data Scientist vs Data Engineer',
+        'post_time': '22/5/2022',
+        'content': 'sasgfhjkkkkkkkkkkklkafaf'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1> Blog home </h1>')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about (request):
-    return HttpResponse('<h1> Blog About </h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
