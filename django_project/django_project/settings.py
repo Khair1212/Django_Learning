fc9cae70bc1662f8718c3386a2ab2d9480c9a5c3
Khair Ahammed <khairahmad6@gmail.com>
2022-05-26 17:57:33 +0600

    Lec 7 Included

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -125,3 +125,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGIN_URL = 'login' 
